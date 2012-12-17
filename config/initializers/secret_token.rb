8fb807d0479d1a91bd21917b57db9fd1d279a51b
SSS <sss@sva.(none)>
2012-12-17 21:25:02 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marina::Application.config.secret_token = 'f898a45fa78146ace5377a29cf7e43a09ca9edf77706e7ea57ed350304e7de8542c3fb91f3224b39eb8c1c967c5b4b58fe46ffef4d3b53810ea5050511c34361'
